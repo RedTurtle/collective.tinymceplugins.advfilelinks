0544bb519f9cf1382592f725f9d707bc3a105327
Luca Fabbri <luca@keul.it>
2014-06-20 09:48:58 +0200

    upgrade also reainstall mtrsetup (just in case you upgraded to 1.5.2)

--- collective/tinymceplugins/advfilelinks/setuphandlers.py
+++ collective/tinymceplugins/advfilelinks/setuphandlers.py
@@ -3,9 +3,9 @@
 from collective.tinymceplugins.advfilelinks import logger
 from Products.CMFCore.utils import getToolByName
 
+
 def migrateTo_0_2_0(context):
     setup_tool = getToolByName(context, 'portal_setup')
-#    setup_tool.setBaselineContext('profile-collective.tinymceplugins.advfilelinks:to_0_2_0')
     setup_tool.runAllImportStepsFromProfile('profile-collective.tinymceplugins.advfilelinks:to_0_2_0')
     setup_tool.runImportStepFromProfile('profile-collective.tinymceplugins.advfilelinks:default', 'browserlayer')
     logger.info("Migrated to version 0.2")
@@ -19,5 +19,7 @@ def migrateTo_1_3_0(context):
 
 def migrateTo_1_3_1(context):
     setup_tool = getToolByName(context, 'portal_setup')
+    # this below it's not exactly fair, but meanwhile versione 1.5.2 or mtrsetup has been released
+    setup_tool.runAllImportStepsFromProfile('profile-collective.mtrsetup:example')
     setup_tool.runImportStepFromProfile('profile-collective.tinymceplugins.advfilelinks:default', 'mimetypes')
     logger.info("Migrated to version 1.3.1")
